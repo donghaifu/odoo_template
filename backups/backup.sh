7dec171b813778240f479552c0cf0b83b90bc508
fu donghai <fudonghai@yeah.net>
2020-06-21 18:56:51 +0800

    添加注意

--- backups/backup.sh
+++ backups/backup.sh
@@ -1,3 +1,4 @@
 Linux使用crontab执行计划任务
+把备份的文件夹给与postgres用户访问权限
 @daily su postgres -c pg_dumpall | gzip > /home/odoo/project/backups/postgresql-$(date +%u).dump.gz
 @daily tar czf /home/odoo/project/backups/odoo-filestore-$(date +%u).tgz  /home/odoo/project/filestore
